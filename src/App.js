16f7e2b70983ef7c6587e86209b034727e7c89b2
ismailarilik <arilik.ismail@gmail.com>
2022-10-12 17:51:42 +0300

    Show a loading indicator between navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { useState } from 'react'
+import { useNavigation } from 'react-router-dom'
 import ReactTooltip from 'react-tooltip'
 import MapChart from './MapChart'
 
 function App() {
   const [content, setContent] = useState('')
+  const navigation = useNavigation()
 
-  return (
-    <div>
-      <MapChart setTooltipContent={setContent} />
-      <ReactTooltip>{content}</ReactTooltip>
-    </div>
-  );
+  if (navigation.state === 'loading') {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div>
+        <MapChart setTooltipContent={setContent} />
+        <ReactTooltip>{content}</ReactTooltip>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/CovidData.js
+++ src/CovidData.js
@@ -1,4 +1,4 @@
-import { useLoaderData } from 'react-router-dom'
+import { useLoaderData, useNavigation } from 'react-router-dom'
 
 const loader = async ({ params }) => {
   return fetch(
@@ -14,17 +14,22 @@ const loader = async ({ params }) => {
 
 const CovidData = () => {
   const covidData = useLoaderData()
+  const navigation = useNavigation()
 
-  return (
-    <div>
-      <div>Recovered: {covidData.data.recovered}</div>
-      <div>Deaths: {covidData.data.deaths}</div>
-      <div>Confirmed: {covidData.data.confirmed}</div>
-      <div>Last checked: {covidData.data.lastChecked}</div>
-      <div>Last reported: {covidData.data.lastReported}</div>
-      <div>Location: {covidData.data.location}</div>
-    </div>
-  );
+  if (navigation.state === 'loading') {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div>
+        <div>Recovered: {covidData.data.recovered}</div>
+        <div>Deaths: {covidData.data.deaths}</div>
+        <div>Confirmed: {covidData.data.confirmed}</div>
+        <div>Last checked: {covidData.data.lastChecked}</div>
+        <div>Last reported: {covidData.data.lastReported}</div>
+        <div>Location: {covidData.data.location}</div>
+      </div>
+    )
+  }
 }
 
 export default CovidData;
