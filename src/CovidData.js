395102612ac608223a36616cabfd0a1545b41d26
ismailarilik <arilik.ismail@gmail.com>
2022-10-14 11:58:13 +0300

    Style loading indicator

--- src/CovidData.js
+++ src/CovidData.js
@@ -22,7 +22,11 @@ const CovidData = () => {
   }
 
   if (navigation.state === 'loading') {
-    return <div>Loading...</div>
+    return (
+      <div className="flex items-center justify-center h-screen">
+          Loading...
+      </div>
+    )
   } else {
     return (
       <div>
