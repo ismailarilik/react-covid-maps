80a51b8d14eeab40d49569a5bfa7311d2d90eea3
ismailarilik <arilik.ismail@gmail.com>
2022-10-14 11:59:17 +0300

    Use h-screen instead of h-full for error indicator
    h-full doesn't work for some scenarios

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -5,7 +5,7 @@ const ErrorPage = () => {
   console.error(error)
 
   return (
-    <div id="error-page" className="flex flex-col items-center justify-center gap-y-3 h-full">
+    <div id="error-page" className="flex flex-col items-center justify-center gap-y-3 h-screen">
       <div className="text-4xl font-bold">Oops!</div>
       <div>Sorry, an unexpected error has occurred.</div>
       <div className="italic">{error.statusText || error.message}</div>
