22734d12d66a002518d5cf512a1b3b99784f6ed6
ismailarilik <arilik.ismail@gmail.com>
2022-10-14 10:56:39 +0300

    Style error page with Tailwind CSS

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -5,12 +5,10 @@ const ErrorPage = () => {
   console.error(error)
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div id="error-page" className="flex flex-col items-center justify-center gap-y-3 h-full">
+      <div className="text-4xl font-bold">Oops!</div>
+      <div>Sorry, an unexpected error has occurred.</div>
+      <div className="italic">{error.statusText || error.message}</div>
     </div>
   )
 }
